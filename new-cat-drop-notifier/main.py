027e141520e47cef2787d4b0c11c34a30b59a75c
ryandemarigny <ryandemarigny@gmail.com>
2022-08-14 22:09:34 -0700

    Proof of concept done

--- new-cat-drop-notifier/main.py
+++ new-cat-drop-notifier/main.py
@@ -1,5 +1,14 @@
+from requests import get
+from bs4 import BeautifulSoup
+
+
 def main():
-    print("Hello World!")
+    params = {"type": "cats"}
+    response = get("https://www.oregonhumane.org/adopt/", params)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    cat_results = soup.find_all("div", {"class": "result-item"})
+    cat_names = [cat_result["data-ohssb-name"] for cat_result in cat_results]
+    print(cat_names)
 
 
 if __name__ == '__main__':
