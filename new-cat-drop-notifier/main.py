b251cd3721d0cc08f3fc92364b839b67b28577ad
ryandemarigny <ryandemarigny@gmail.com>
2022-08-16 07:03:39 -0700

    Added some logging

--- new-cat-drop-notifier/main.py
+++ new-cat-drop-notifier/main.py
@@ -1,7 +1,11 @@
+from logging import getLogger, basicConfig, INFO
 from os import getenv
 from time import sleep
 
 from interfaces import OHSAdoptPage, SMSMessenger
+
+basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level=INFO)
+logger = getLogger(__name__)
 
 
 def main():
@@ -10,10 +14,12 @@ def main():
     sms_messenger = SMSMessenger()
 
     while True:
+        logger.info("Checking for a cat drop...")
         cat_adoption_page = OHSAdoptPage("cats")
         new_cats = list(set(cat_adoption_page.all_animals) - set(cats_last_seen))
 
         if new_cats:
+            logger.info(f"NEW CAT DROP! UOC (Units of Cat): {len(new_cats)}")
             message = "NEW CAT DROP!\n"
             for cat in new_cats:
                 if len(message + str(cat)) >= 1600:
@@ -21,8 +27,11 @@ def main():
                     message = ""
                 message += str(cat)
             sms_messenger.send_sms(message)
+        else:
+            logger.info("No cat drop!")
 
         cats_last_seen = cat_adoption_page.all_animals
+        logger.info(f"Sleeping for {delay//60} minutes")
         sleep(delay)
 
 
